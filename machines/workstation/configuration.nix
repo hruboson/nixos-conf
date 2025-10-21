c0f158b34ae642133fa5c359dcf8eac941e506c7
Ondřej Hruboš <hruboson@gmail.com>
2025-10-21 14:18:08 +0200

    Plasma settings, apps, keyboard

--- machines/workstation/configuration.nix
+++ machines/workstation/configuration.nix
@@ -13,13 +13,38 @@
 	networking.hostName = "hruon-nixos-ws";
 
 	# DESKTOP ENVIRONMENT
-	services.xserver.enable = true;
 	services.displayManager.sddm.enable = true;
 	services.desktopManager.plasma6.enable = true;
 	security.rtkit.enable = true;
 
-	services.xserver.libinput.enable = true; # enable touchpad
+	services.libinput.enable = true; # enable touchpad
+	services.xserver = { # keyboard settings
+		enable = true;
+		layout = "cz,us";
+		xkbVariant = "winkeys";
+		xkbOptions = "grp:shift_space_toggle";
+	};
 
+	services.xserver.displayManager.sddm.settings = {
+		X11 = {
+			KeyboardLayout = "cz,us";
+		};
+	};
+
+	## Excluded packages (should produce minimal KDE environment)
+	environment.plasma6.excludePackages = with pkgs.kdePackages; [
+		dolphin 		# file manager
+		konsole			# terminal
+		elisa			# music player
+		# ocular		# document viewer (kept, this one is actually quite nice)
+		kdenlive		# video editor
+		k3b				# cd/dvd burner
+		# gwenview		# image viewer (kept)
+		kmail			# mail client
+		#korganizer		# calendar (kept)
+	];
+
+	## DE-related packages
 	environment.systemPackages = lib.mkAfter (with pkgs; [
 			wl-clipboard
 			wayland-utils
