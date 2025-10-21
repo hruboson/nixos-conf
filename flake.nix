6d1e8fc63bcf42aa2cb48017a7ee0a185ff5da74
Ondřej Hruboš <hruboson@gmail.com>
2025-10-21 14:24:56 +0200

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-		home-manager.url = "github:nix-community/home-manager/release-25.05";
-		home-manager.inputs.nixpkgs.follows = "nixkpkgs";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-25.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
 	outputs = { self, nixpkgs, home-manager, ... }@inputs:
