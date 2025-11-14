34355fb47332ffd5e647ce427c79f1593c4ea063
Ondřej Hruboš <hruboson@gmail.com>
2025-11-14 18:01:46 +0100

    Greetd and tuigreet config

--- machines/workstation/configuration.nix
+++ machines/workstation/configuration.nix
@@ -14,20 +14,33 @@
 	networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 	networking.hostName = hostname;
 
+	# graphical login screen (greetd+tuigreet)
+	services.greetd = {
+		enable = true;
+		settings = {
+			default_session = {
+				command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+				user = "greeter";
+			};
+		};
+	};
+
 	programs.sway.enable = true;
 	services.xserver.enable = false; # disable X11
 	security.polkit.enable = true;
 
 	# for QEMU
 	services.xserver.videoDrivers = [ "virtio" ];
+	environment.variables.WLR_NO_HARDWARE_CURSORS = "1";
 
 	# enable hardware acceleration
-	hardware.opengl = {
+	hardware.graphics = {
 		enable = true;
 	};
 
 	# sway utils
 	environment.systemPackages = lib.mkAfter(with pkgs; [
+		greetd.tuigreet
 		wayland
 		wlr-randr
 		swaybg
