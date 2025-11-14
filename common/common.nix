01bf34837595c5199fc55c5e979226cb911c15fc
Ondřej Hruboš <hruboson@gmail.com>
2025-11-14 07:20:16 +0100

    Add locale settings

--- common/common.nix
+++ common/common.nix
@@ -22,6 +22,20 @@
 	};
 	time.timeZone = "Europe/Prague";
 
+	i18n.defaultLocale = "en_US.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "cs_CZ.UTF-8";
+		LC_IDENTIFICATION = "cs_CZ.UTF-8";
+		LC_MEASUREMENT = "cs_CZ.UTF-8";
+		LC_MONETARY = "cs_CZ.UTF-8";
+		LC_NAME = "cs_CZ.UTF-8";
+		LC_NUMERIC = "cs_CZ.UTF-8";
+		LC_PAPER = "cs_CZ.UTF-8";
+		LC_TELEPHONE = "cs_CZ.UTF-8";
+		LC_TIME = "cs_CZ.UTF-8";
+	};
+
+
 	# NETWORK
 	#networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 	networking.firewall.enable = true;
