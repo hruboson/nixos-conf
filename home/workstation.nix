f43472ba062f5f062557ec303f772e834913f8a6
Ondřej Hruboš <hruboson@gmail.com>
2025-12-13 13:20:04 +0100

    Cursor theme in home manager

--- home/workstation.nix
+++ home/workstation.nix
@@ -33,6 +33,7 @@
 	'';
 
 	# HYPRLAND PLUGINS
+	# currently managed in systemwide config
 	/*wayland.windowManager.hyprland = {
 		enable = true; # allows home-manager to configure hyprland
 		package = inputs.hyprland.packages."${pkgs.system}".hyprland;
@@ -69,4 +70,25 @@
 			};
 		};
 	};*/
+
+	home.pointerCursor = let 
+		getFrom = url: hash: name: {
+			gtk.enable = true;
+			x11.enable = true;
+			name = name;
+			size = 48;
+			package = 
+				pkgs.runCommand "moveUp" {} ''
+				mkdir -p $out/share/icons
+				ln -s ${pkgs.fetchzip {
+					url = url;
+					hash = hash;
+				}} $out/share/icons/${name}
+			'';
+		};
+	in
+		getFrom 
+		"https://github.com/polirritmico/Breeze-Dark-Cursor/releases/download/v1.0/Breeze_Dark_v1.0.tar.gz"
+		"sha256-FgqS3rHJ4o5x4ONSaDZlQu1sFhefhWPk8vaBvURKZzY=" # get this hash by running nix store prefetch-file https://github/cursortheme/theme.tar.gz ... if this hash is wrong you wont be able to rebuild the system
+		"Breeze-Dark";
 }
