afd9cd9f3a1dc5b6176c04d4a87c7ea7affb1825
Ondřej Hruboš <hruboson@gmail.com>
2026-01-14 17:22:04 +0100

    Eww portals v0.0.1

--- dotfiles/eww/scripts/toggle_portals.sh
+++ dotfiles/eww/scripts/toggle_portals.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if eww active-windows | grep -q "portals_window_left" ; then
+	eww update rev=false
 	eww close portals_window_left
 	eww close portals_window_right
 else
 	eww open portals_window_left
 	eww open portals_window_right
+	eww update rev=true
 fi
