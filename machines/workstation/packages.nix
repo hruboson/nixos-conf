b7e9b6169674c8bcaae0ff9d15b87bf4559db91e
Ondřej Hruboš <hruboson@gmail.com>
2026-03-18 13:06:07 +0100

    Trying to fix android connection

--- machines/workstation/packages.nix
+++ machines/workstation/packages.nix
@@ -16,7 +16,7 @@
 		udiskie  # Auto-mount daemon
 		playerctl # Audio control daemon
 		polychromatic # Razer devices color management
-		jmtpfs libmtp android-tools # Media Transfer Protocol (for Android devices)
+		jmtpfs libmtp android-tools usbutils libmtp go-mtpfs # Media Transfer Protocol (for Android devices)
 
 		# utils
 		doublecmd
