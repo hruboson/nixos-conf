39ce098790b7224684b96531482cd4d198483f62
Ondřej Hruboš <hruboson@gmail.com>
2026-04-09 22:51:44 +0200

    Mango: remove home-manager code that doesnt work

--- parts/desktops/mango/default.nix
+++ parts/desktops/mango/default.nix
@@ -452,27 +452,6 @@
 			home-manager.users.${username} = {
 				imports = [ inputs.mango.hmModules.mango ];
 
-				home.pointerCursor = let 
-					getFrom = url: hash: name: {
-						gtk.enable = true;
-						x11.enable = true;
-						name = name;
-						size = 48;
-						package = 
-							pkgs.runCommand "moveUp" {} ''
-							mkdir -p $out/share/icons
-							ln -s ${pkgs.fetchzip {
-								url = url;
-								hash = hash;
-							}} $out/share/icons/${name}
-						'';
-					};
-				in
-					getFrom 
-					"https://github.com/polirritmico/Breeze-Dark-Cursor/releases/download/v1.0/Breeze_Dark_v1.0.tar.gz"
-					"sha256-FgqS3rHJ4o5x4ONSaDZlQu1sFhefhWPk8vaBvURKZzY=" # get this hash by running nix store prefetch-file https://github/cursortheme/theme.tar.gz ... if this hash is wrong you wont be able to rebuild the system
-					"Breeze-Dark";
-
 				wayland.windowManager.mango = {
 					enable = true;
 
@@ -733,4 +712,4 @@
 			};
 		};
 	};
-					   }
+}
