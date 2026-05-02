23b43a7dd7bc394e0a6b6bc4ff91f6829252a21c
Ondřej Hruboš <hruboson@gmail.com>
2026-05-02 19:44:34 +0200

    Packs: heroic and hw fix

--- parts/programs/packs/games.nix
+++ parts/programs/packs/games.nix
@@ -1,15 +1,28 @@
 { self, inputs, ... }: {
 	flake.nixosModules.appPackGames = { config, lib, pkgs, username, ... }: {
-		environment.systemPackages = with pkgs; [
+		environment.systemPackages = with pkgs; let
+		heroicWithExtras = pkgs.heroic.override {
+			extraPkgs = pkgs': with pkgs'; [
+				gamescope
+				gamemode
+				wineWowPackages.stable
+				winetricks
+			];
+		};
+		in 
+		[
 			prismlauncher # minecraft foss launcher
 			osu-lazer-bin # click the circles
-			heroic # epic games + steam + gog in one app
+			heroicWithExtras # epic games + steam + gog in one app
 			pcsx2 # PS2 emulator
 			lutris # Windows games emulator
 			bottles # can run windows games if you have the .exe file
+
+			libretro.beetle-psx-hw # possible ps5 controller fix
 		];
 
 		programs.gamemode.enable = true;
+		programs.gamescope.enable = true;
 		programs.steam = {
 			enable = true;
 			remotePlay.openFirewall = true;
@@ -18,6 +31,8 @@
 				proton-ge-bin
 			];
 		};
+		hardware.steam-hardware.enable = true;
+		hardware.xone.enable = true;
 
 		home-manager.users.${username} = {
 			# File types -> app associations
