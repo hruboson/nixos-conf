d682b8ce2dbefc80654210bb946b67ac5ed11e5e
Ondřej Hruboš <hruboson@gmail.com>
2026-06-30 17:36:42 +0200

    Packs: add Appimage for desktop

--- parts/programs/packs/desktop.nix
+++ parts/programs/packs/desktop.nix
@@ -1,122 +1,140 @@
 { self, inputs, ... }: {
-	flake.nixosModules.appPackDesktop = { config, lib, pkgs, username, ... }: {
-		nixpkgs.config.allowUnfree = true;
+  flake.nixosModules.appPackDesktop =
+    {
+      config,
+      lib,
+      pkgs,
+      username,
+      ...
+    }:
+    {
+      nixpkgs.config.allowUnfree = true;
 
-		environment.systemPackages = with pkgs; let
-			application-menu = pkgs.runCommandLocal "xdg-application-menu" { } ''
-				mkdir -p $out/etc/xdg/menus/
-				ln -s ${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu \
-				$out/etc/xdg/menus/applications.menu
-			'';
-		in [
-			# files
-			kdePackages.filelight
-			kdePackages.ark
-			kdePackages.dolphin kdePackages.ffmpegthumbs kdePackages.qtimageformats
-			application-menu
-			android-file-transfer
+      environment.systemPackages =
+        with pkgs;
+        let
+          application-menu = pkgs.runCommandLocal "xdg-application-menu" { } ''
+            mkdir -p $out/etc/xdg/menus/
+            ln -s ${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu \
+            $out/etc/xdg/menus/applications.menu
+          '';
+        in
+        [
+          # files
+          kdePackages.filelight
+          kdePackages.ark
+          kdePackages.dolphin
+          kdePackages.ffmpegthumbs
+          kdePackages.qtimageformats
+          application-menu
+          android-file-transfer
 
-			# img, video, audio
-			oculante
-			gthumb
-			vlc
-			spotify
-			quodlibet
-			
-			# emulator
-			bottles
+          # img, video, audio
+          oculante
+          gthumb
+          vlc
+          spotify
+          quodlibet
 
-			# communications
-			#discord -> now managed through HM
-			element-desktop
-			
-			# browser
-			firefox
-			floorp-bin
+          # emulator
+          bottles
 
-			# office
-			libreoffice-fresh
+          # communications
+          #discord -> now managed through HM
+          element-desktop
 
-			# other
-			gnome-software
-			
-			# archive backends for ark
-			unrar
-			p7zip
-			libarchive
-			zip
-			unzip
-			gnutar
-			gzip
-			bzip2
-			xz
-			zstd
-			lrzip
-			lzop
-		];
+          # browser
+          firefox
+          floorp-bin
 
-		# enable flatpak (for imperative installs -- sure, go for it)
-		services.flatpak.enable = true;
-		systemd.services.flatpak-repo = {
-			wantedBy = [ "multi-user.target" ];
-			path = [ pkgs.flatpak ];
-			script = ''
-				flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-			'';
-		};
+          # office
+          libreoffice-fresh
 
-		# should fix kde unpopulated xdg mime apps menu
-		environment.etc."/xdg/menus/applications.menu".text = builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
-		xdg.mime.enable = true;
-		xdg.menus.enable = true;
-		xdg.portal.enable = true;
+          # other
+          gnome-software
 
-		home-manager.users.${username} = {
-			nixpkgs.config.allowUnfree = true;
-			programs.discord.enable = true;
+          # archive backends for ark
+          unrar
+          p7zip
+          libarchive
+          zip
+          unzip
+          gnutar
+          gzip
+          bzip2
+          xz
+          zstd
+          lrzip
+          lzop
+        ];
 
-			# File types -> app associations
-			# mimetype.io/all-types
-			xdg.mimeApps = {
-				enable = true;
-				defaultApplications = {
-					# Images
-					"image/jpeg" = "oculante.desktop";
-					"image/png" = "oculante.desktop";
-					"image/webp" = "oculante.desktop";
-					"image/gif" = "oculante.desktop";
-					"image/bmp" = "oculante.desktop";
-					"image/tiff" = "oculante.desktop";
-					"image/svg+xml" = "oculante.desktop";
+      # enable flatpak (for imperative installs -- sure, go for it)
+      services.flatpak.enable = true;
+      systemd.services.flatpak-repo = {
+        wantedBy = [ "multi-user.target" ];
+        path = [ pkgs.flatpak ];
+        script = ''
+          flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+        '';
+      };
+      programs.appimage = {
+        enable = true;
+        binfmt = true;
+      };
 
-					# Directories
-					"inode/directory" = "org.kde.dolphin.desktop";
+      # should fix kde unpopulated xdg mime apps menu
+      environment.etc."/xdg/menus/applications.menu".text =
+        builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+      xdg.mime.enable = true;
+      xdg.menus.enable = true;
+      xdg.portal.enable = true;
 
-					# Links
-					"text/html"                = "firefox.desktop";
-					"x-scheme-handler/http"   = "firefox.desktop";
-					"x-scheme-handler/https"  = "firefox.desktop";
-					"x-scheme-handler/about"  = "firefox.desktop";
-					"x-scheme-handler/unknown" = "firefox.desktop";
+      home-manager.users.${username} = {
+        nixpkgs.config.allowUnfree = true;
+        programs.discord.enable = true;
 
-					# Documents
-					"application/pdf" = "firefox.desktop";
-					"text/markdown" = "firefox.desktop";
+        # File types -> app associations
+        # mimetype.io/all-types
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            # Images
+            "image/jpeg" = "oculante.desktop";
+            "image/png" = "oculante.desktop";
+            "image/webp" = "oculante.desktop";
+            "image/gif" = "oculante.desktop";
+            "image/bmp" = "oculante.desktop";
+            "image/tiff" = "oculante.desktop";
+            "image/svg+xml" = "oculante.desktop";
 
-					# Archives -> PeaZip
-					"application/zip" = "org.kde.ark.desktop";
-					"application/x-zip-compressed" = "org.kde.ark.desktop";
-					"application/x-rar" = "org.kde.ark.desktop";
-					"application/vnd.rar" = "org.kde.ark.desktop";
-					"application/x-7z-compressed" = "org.kde.ark.desktop";
-					"application/x-tar" = "org.kde.ark.desktop";
-					"application/gzip" = "org.kde.ark.desktop";
-					"application/x-gzip" = "org.kde.ark.desktop";
-					"application/x-bzip2" = "org.kde.ark.desktop";
-					"application/x-xz" = "org.kde.ark.desktop";
-					"application/zstd" = "org.kde.ark.desktop";
-				};
-			};
-		};
-	};
+            # Directories
+            "inode/directory" = "org.kde.dolphin.desktop";
+
+            # Links
+            "text/html" = "firefox.desktop";
+            "x-scheme-handler/http" = "firefox.desktop";
+            "x-scheme-handler/https" = "firefox.desktop";
+            "x-scheme-handler/about" = "firefox.desktop";
+            "x-scheme-handler/unknown" = "firefox.desktop";
+
+            # Documents
+            "application/pdf" = "firefox.desktop";
+            "text/markdown" = "firefox.desktop";
+
+            # Archives -> PeaZip
+            "application/zip" = "org.kde.ark.desktop";
+            "application/x-zip-compressed" = "org.kde.ark.desktop";
+            "application/x-rar" = "org.kde.ark.desktop";
+            "application/vnd.rar" = "org.kde.ark.desktop";
+            "application/x-7z-compressed" = "org.kde.ark.desktop";
+            "application/x-tar" = "org.kde.ark.desktop";
+            "application/gzip" = "org.kde.ark.desktop";
+            "application/x-gzip" = "org.kde.ark.desktop";
+            "application/x-bzip2" = "org.kde.ark.desktop";
+            "application/x-xz" = "org.kde.ark.desktop";
+            "application/zstd" = "org.kde.ark.desktop";
+          };
+        };
+      };
+    };
 }
