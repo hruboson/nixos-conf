2c10b475cefeace0d1deff6e1d2892b57c60d770
hruboson <hruboson@gmail.com>
2026-07-02 11:17:54 +0200

    Forgejo: move data dir, fix ssh ports

--- parts/services/selfhosted/forgejo/default.nix
+++ parts/services/selfhosted/forgejo/default.nix
@@ -1,21 +1,26 @@
 { self, inputs, ... }: {
 	flake.nixosModules.selfhostedForgejo = { config, lib, pkgs, username, ... }: {
+		services.openssh.enable = true;
+		services.openssh.ports = [ 2222 ];
 		services.forgejo = {
 			enable = true;
 			lfs.enable = true;
 
 			database.type = "postgres";
+			stateDir = "/mnt/HUSK/forgejo";
 
 			settings = {
 				server = {
 					DOMAIN = "${config.networking.hostName}.local";
 					ROOT_URL = "http://${config.networking.hostName}.local:2020/";
-					HTTP_PORT = 2020;
-					SSH_DOMAIN = "${config.networking.hostName}.local";
 					HTTP_ADDR = "0.0.0.0";
+					HTTP_PORT = 2020;
+
+					SSH_DOMAIN = "${config.networking.hostName}.local";
+					SSH_PORT = lib.head config.services.openssh.ports;
 				};
 				service = {
-					DISABLE_REGISTRATION = false; # false only when registering admin for the first time
+					DISABLE_REGISTRATION = true; # false only when registering admin for the first time
 				};
 			};
 		};
