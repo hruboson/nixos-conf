fe0f36e8b34bfbd8db230a4558573afaa15ebd8e
Ondřej Hruboš <hruboson@gmail.com>
2026-07-10 15:28:15 +0200

    Packs: games - add retroarch for PS1 emulation

--- parts/programs/packs/games.nix
+++ parts/programs/packs/games.nix
@@ -1,48 +1,65 @@
 { self, inputs, ... }: {
-	flake.nixosModules.appPackGames = { config, lib, pkgs, username, ... }: {
-		environment.systemPackages = with pkgs; let
-		heroicWithExtras = pkgs.heroic.override {
-			extraPkgs = pkgs': with pkgs'; [
-				gamescope
-				gamemode
-				wineWow64Packages.stable
-				winetricks
-			];
-		};
-		in 
-		[
-			prismlauncher # minecraft foss launcher
-			osu-lazer-bin # click the circles
-			heroicWithExtras # epic games + steam + gog in one app
-			pcsx2 # PS2 emulator
-			lutris # Windows games emulator
-			bottles # can run windows games if you have the .exe file
+  flake.nixosModules.appPackGames =
+    {
+      config,
+      lib,
+      pkgs,
+      username,
+      ...
+    }:
+    {
+      environment.systemPackages =
+        with pkgs;
+        let
+          heroicWithExtras = pkgs.heroic.override {
+            extraPkgs =
+              pkgs': with pkgs'; [
+                gamescope
+                gamemode
+                wineWow64Packages.stable
+                winetricks
+              ];
+          };
+        in
+        [
+          prismlauncher # minecraft foss launcher
+          osu-lazer-bin # click the circles
+          heroicWithExtras # epic games + steam + gog in one app
+          lutris # Windows games emulator
+          bottles # can run windows games if you have the .exe file
 
-			libretro.beetle-psx-hw # possible ps5 controller fix
-		];
+          libretro.beetle-psx-hw # possible ps5 controller fix
 
-		programs.gamemode.enable = true;
-		programs.gamescope.enable = true;
-		programs.steam = {
-			enable = true;
-			remotePlay.openFirewall = true;
-			dedicatedServer.openFirewall = true;
-			extraCompatPackages = with pkgs; [
-				proton-ge-bin
-			];
-		};
-		hardware.steam-hardware.enable = true;
-		hardware.xone.enable = true;
+          pcsx2 # PS2 emulator
+          (retroarch.withCores (
+            cores: with cores; [
+              beetle-psx-hw # PS1 emulator
+            ]
+          ))
+        ];
 
-		home-manager.users.${username} = {
-			# File types -> app associations
-			# mimetype.io/all-types
-			xdg.mimeApps = {
-				enable = true;
-				defaultApplications = {
-					
-				};
-			};
-		};
-	};
+      programs.gamemode.enable = true;
+      programs.gamescope.enable = true;
+      programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
+      };
+      hardware.steam-hardware.enable = true;
+      hardware.xone.enable = true;
+
+      home-manager.users.${username} = {
+        # File types -> app associations
+        # mimetype.io/all-types
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+
+          };
+        };
+      };
+    };
 }
