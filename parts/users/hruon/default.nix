8ef61f24aa22a97b7f5040402be7f8a19e3c0fd2
Ondřej Hruboš <hruboson@gmail.com>
2026-08-14 16:22:20 +0200

    User: add pubkey and trusted-users for remote nixos-rebuild

--- parts/users/hruon/default.nix
+++ parts/users/hruon/default.nix
@@ -1,40 +1,63 @@
 { self, inputs, ... }: {
-	flake.nixosModules.users = { pkgs, lib, config, username, ... }: let 
-		userMail = "hruboson@gmail.com";
-		userName = "Ondřej Hruboš";
-	in {
-		users.groups.media = {}; # group for external drives that need both services and user access
-		users.users.${username} = {
-			isNormalUser = true;
-			extraGroups = [ "wheel" "networkmanager" "video" "audio" "media" "adbusers" "dialout" ];
-		};
+  flake.nixosModules.users =
+    {
+      pkgs,
+      lib,
+      config,
+      username,
+      ...
+    }:
+    let
+      userMail = "hruboson@gmail.com";
+      userName = "Ondřej Hruboš";
+    in
+    {
+      users.groups.media = { }; # group for external drives that need both services and user access
+      users.users.${username} = {
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+          "video"
+          "audio"
+          "media"
+          "adbusers"
+          "dialout"
+        ];
 
-		home-manager.users.${username} = {
-			home.username = username;
-			home.stateVersion = "26.05";  # set this to your current nixpkgs version and never change it
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPjoRbd+2itXlw3mSVS7BfPj/SKVXME6Jvdk8IJCAddl" # tropikey pubkey
+        ];
+      };
 
-			programs.lazygit.enable = true;
-			programs.git = {
-				enable = true;
-				settings = {
-					credential.helper = "${pkgs.gh}/bin/gh auth git-credential";
-					user = {
-						name = userName;
-						email = userMail;
-						init.defaultBranch = "main";
-						pull.rebase = true;
-					};
-				};
-			};
-			programs.jujutsu = {
-				enable = true;
-				settings = {
-					user = {
-						name = userName;
-						email = userMail;
-					};
-				};
-			};
-		};
-	};
+	  nix.settings.trusted-users = [ "root" "${username}" ];
+
+      home-manager.users.${username} = {
+        home.username = username;
+        home.stateVersion = "26.05"; # set this to your current nixpkgs version and never change it
+
+        programs.lazygit.enable = true;
+        programs.git = {
+          enable = true;
+          settings = {
+            credential.helper = "${pkgs.gh}/bin/gh auth git-credential";
+            user = {
+              name = userName;
+              email = userMail;
+              init.defaultBranch = "main";
+              pull.rebase = true;
+            };
+          };
+        };
+        programs.jujutsu = {
+          enable = true;
+          settings = {
+            user = {
+              name = userName;
+              email = userMail;
+            };
+          };
+        };
+      };
+    };
 }
