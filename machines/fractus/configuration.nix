37b2cc2d7aefeb272fcec5710aeb6c5d6c054262
Ondřej Hruboš <hruboson@gmail.com>
2026-09-13 10:43:44 +0200

    Fractus: update

--- machines/fractus/configuration.nix
+++ machines/fractus/configuration.nix
@@ -13,8 +13,10 @@
         self.nixosModules.fractusSystem
         self.nixosModules.users
 
+        self.nixosModules.desktopOptions
         self.nixosModules.mango
         self.nixosModules.kitty
+
         self.nixosModules.appPackDev
         self.nixosModules.appPackNetworking
         self.nixosModules.appPackSysutils
@@ -40,13 +42,14 @@
         flake = "/home/${username}/nixos-conf"; # sets NH_OS_FLAKE
       };
 
+      desktops.session = "mango";
       desktops.mango.monitors = ''
         monitorrule=name:eDP-1,width:1920,height:1080,refresh:60,x:0,y:0,scale:1
       '';
       desktops.lockscreen.background = pkgs.fetchurl {
-        name = "moody_blues.webp";
-        url = "https://raw.githubusercontent.com/hruboson/wallpapers/refs/heads/main/jjba/jjba_moody_blues.webp";
-        hash = "sha256-lMTHuOt4fFEMcRr7kaBYxpaOfhyOmXrDpUbaV9vMDDc=";
+        name = "japan_art_walkway.png";
+        url = "https://raw.githubusercontent.com/hruboson/wallpapers/refs/heads/main/japan/art/japan_art_walkway.jpg";
+        hash = "sha256-eXWP1bF9NepMjiPrKWLklhOD7rxjqbO1jmU2Uns//sE=";
       };
 
       desktops.lockscreen.profilePicture = pkgs.fetchurl {
